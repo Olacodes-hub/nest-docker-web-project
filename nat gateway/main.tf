e1f70a3ad41232f37d6bfa4d462689d0c8e40d74
“safo” <lekanaws@gmail.com>
2023-10-06 09:19:40 -0500

    created nat gateway file

---
+++ nat gateway/main.tf
@@ -0,0 +1,97 @@
+
+# create elastic ip for nat gateway az1
+resource "aws_eip" "eip_for_nat_gateway_az1" {
+  vpc = true
+
+  tags = {
+    Name = "eip for nat gateway az1"
+  }
+}
+
+# create elastic ip for nat gateway az2
+resource "aws_eip" "eip_for_nat_gateway_az2" {
+  vpc = true
+
+  tags = {
+    Name = "eip for nat gateway az1"
+  }
+}
+
+# create nat gateway for az1
+resource "aws_nat_gateway" "nat_gateway_az1" {
+  allocation_id = aws_eip.eip_for_nat_gateway_az1.id
+  subnet_id     = var.public_subnet_az1_id
+
+  tags = {
+    Name = "nat gateway az1"
+  }
+
+  depends_on = [var.internet_gateway]
+
+}
+
+# create nat gateway for az2
+resource "aws_nat_gateway" "nat_gateway_az2" {
+  allocation_id = aws_eip.eip_for_nat_gateway_az2.id
+  subnet_id     = var.public_subnet_az2_id
+
+  tags = {
+    Name = "nat gateway az2"
+  }
+
+  depends_on = [var.internet_gateway]
+
+}
+
+
+# create private route table az1
+resource "aws_route_table" "private_route_table_az1" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_az1.id
+  }
+
+  tags = {
+    Name = "private route table az1"
+  }
+}
+
+# create private route table az2
+resource "aws_route_table" "private_route_table_az2" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_az2.id
+  }
+
+  tags = {
+    Name = "private route table az2"
+  }
+}
+
+# associate route table with private app subnet AZ1
+resource "aws_route_table_association" "private_app_subnet_az1" {
+  subnet_id      = var.private_app_subnet_az1_id
+  route_table_id = aws_route_table.private_route_table_az1.id
+}
+
+# associate route table with private data subnet AZ2
+resource "aws_route_table_association" "private_data_subnet_az1" {
+  subnet_id      = var.private_data_subnet_az1_id
+  route_table_id = aws_route_table.private_route_table_az1.id
+}
+
+# associate private app subnet az2 with private route table az2
+resource "aws_route_table_association" "private_app_subnet_az2_rt_az2_association" {
+  subnet_id      = var.private_app_subnet_az2_id
+  route_table_id = aws_route_table.private_route_table_az2.id
+}
+
+# associate private data subnet az2 with private route table az2
+resource "aws_route_table_association" "private_data_subnet_az2_rt_az2_association" {
+  subnet_id      = var.private_data_subnet_az2_id
+  route_table_id = aws_route_table.private_route_table_az2.id
+}
--- providers.tf
+++ providers.tf
@@ -1,13 +1,13 @@
 
 # configure aws provider to establish a secure connection between terraform and aws
 provider "aws" {
-  region  = "var.region"
+  region  = "us-east-1"
   profile = "nest1"
 
   default_tags {
     tags = {
-      "Automation"  = terraform
-      "Project"     = var.PROJECT-NAME
+      "Automation"  = "terraform"
+      "Project"     = var.PROJECT_NAME
       "Environment" = var.ENVIRONMENT
     }
   }
