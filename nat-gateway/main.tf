1817ebc035ad7e9b463e71ef6901d067a7d74984
“safo” <lekanaws@gmail.com>
2023-10-06 18:22:09 -0500

    update nat files

--- nat-gateway/main.tf
+++ nat-gateway/main.tf
@@ -59,18 +59,18 @@ resource "aws_route_table" "private_route_table_az1" {
 }
 
 # create private route table az2
-rresource "aws_route_table" "private_route_table_az2" {
-  vpc_id = "10.0.0.0/16"
+#resource "aws_route_table" "private_route_table_az2" {
+ # vpc_id = var.vpc_id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway_az2.id
-  }
+  #route {
+   # cidr_block     = "0.0.0.0/0"
+   # nat_gateway_id = aws_nat_gateway.nat_gateway_az2.id
+  #}
 
-  tags = {
-    Name = "private route table az2"
-  }
-}
+  # tags = {
+   # Name = "private route table az2"
+ # }
+#}
 
 # associate route table with private app subnet AZ1
 resource "aws_route_table_association" "private_app_subnet_az1" {
