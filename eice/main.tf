6464d2e236256a935e3f57728f5ed733edca1578
“safo” <lekanaws@gmail.com>
2023-10-14 23:20:49 -0500

    update

--- eice/main.tf
+++ eice/main.tf
@@ -1,7 +1,7 @@
 # create an ec2 instance connect endpoint
 resource "aws_ec2_instance_connect_endpoint" "instance_connect_endpoint" {
   subnet_id          = var.private_app_subnet_az1_id
-  security_group_ids = [var.eice_security_group_id]
+  security_group_ids = var.eice_security_group_id
   tags = {
     Name = "ec2 connect endpoint"
   }
