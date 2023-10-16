af2493ddbeade4a11c3ffe8225858a990dd86241
“safo” <lekanaws@gmail.com>
2023-10-15 20:52:01 -0500

    modified

--- main.tf
+++ main.tf
@@ -77,7 +77,7 @@ module "eice" {
 }
 module "migrate-data" {
   source                       = "git@github.com:Olacodes-hub/nest-docker-web-project.git//migrate-data"
-  vpc_id = module.vpc.vpc_id
+  vpc_id                       = module.vpc.vpc_id
   amazon_linux_ami_id          = var.amazon_linux_ami_id
   ec2_instance_type            = var.ec2_instance_type
   private_app_subnet_az1_id    = module.vpc.private_app_subnet_az1_id
--- migrate-data/variables.tf
+++ migrate-data/variables.tf
@@ -1,13 +1,59 @@
-variable "vpc_id" {}
-variable "amazon_linux_ami_id" {}
-variable "ec2_instance_type" {}
-variable "private_app_subnet_az1_id" {}
-variable "app_server_security_group_id" {}
-variable "eice_security_group_id" {}
-variable "RDS_ENDPOINT" {}
-variable "RDS_DB_NAME" {}
-variable "USERNAME" {}
-variable "PASSWORD" {}
-variable "PROJECT_NAME" {}
-variable "ENVIRONMENT" {}
-variable "DOMAIN_NAME" {}
+variable "amazon_linux_ami_id" {
+  description = "The ID of the Amazon Linux AMI"
+  type        = string
+}
+
+variable "ec2_instance_type" {
+  description = "The type of EC2 instance"
+  type        = string
+}
+
+variable "private_app_subnet_az1" {
+  description = "The ID of the private app subnet in availability zone 1"
+  type        = string
+}
+
+variable "app_server_security_group_id" {
+  description = "The ID of the app server security group"
+  type        = string
+}
+
+variable "eice_security_group_id" {
+  description = "The ID of the EICE security group"
+  type        = string
+}
+
+variable "RDS_ENDPOINT" {
+  description = "The RDS endpoint"
+  type        = string
+}
+
+variable "RDS_DB_NAME" {
+  description = "The name of the RDS database"
+  type        = string
+}
+
+variable "USERNAME" {
+  description = "The username for something"
+  type        = string
+}
+
+variable "PASSWORD" {
+  description = "The password for something"
+  type        = string
+}
+
+variable "PROJECT_NAME" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "ENVIRONMENT" {
+  description = "The environment of the project"
+  type        = string
+}
+
+variable "DOMAIN_NAME" {
+  description = "The domain name"
+  type        = string
+}
