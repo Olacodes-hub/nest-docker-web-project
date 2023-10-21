655d6707b544151fdadcda795f3187e4f9e48596
“safo” <lekanaws@gmail.com>
2023-10-21 15:29:01 -0500

    updates

--- migrate-data/main.tf
+++ migrate-data/main.tf
@@ -7,15 +7,30 @@ resource "aws_instance" "data_migrate_ec2" {
   iam_instance_profile   = var.iam_instance_profile
 
 
-  user_data = base64encode(templatefile("/Users/safo/nest-docker-web-project/migrate-data/migrate-nest-sql.sh.tpl", {
+  user_data = user_data = base64encode(local_file.user_data_script.content) {
   rds_endpoint = var.rds_endpoint
   rds_db_name  = var.rds_db_name
   username     = var.username
   password     = var.password
-}))
+}
 
 
-  depends_on = [var.rds_instance]
+  depends_on = [aws_db_instance.database_instance]
+
+  tags = {
+    Name = "nest-ec2-migrate"
+  }
+}
+
+resource "aws_instance" "data_migrate_ec2" {
+  ami                    = var.amazon_linux_ami_id
+  instance_type          = var.ec2_instance_type
+  subnet_id              = var.private_app_subnet_az1.id
+  vpc_security_group_ids = [var.app_server_security_group_id, var.eice_security_group_id]
+  iam_instance_profile   = var.iam_instance_profile
+
+  user_data = base64encode(local_file.user_data_script.content)
+  
 
   tags = {
     Name = "nest-ec2-migrate"
--- rds/output.tf
+++ rds/output.tf
@@ -2,8 +2,3 @@ output "rds_endpoint" {
   value = aws_db_instance.database_instance.endpoint
   
 }
-
-output "rds_instance" {
-  value = aws_db_instance.database_instance.id
-  
-}
