f6a496f5a6cebb214d97f8d60afcbc9323a49588
“safo” <lekanaws@gmail.com>
2023-10-21 17:27:00 -0500

    y

--- migrate-data/main.tf
+++ migrate-data/main.tf
@@ -1,9 +1,3 @@
-
-data "aws_db_instance" "external_rds" {
-  instance_id = aws_db_instance.database_instance.id
-}
-
-
 resource "aws_instance" "data_migrate_ec2" {
   ami                    = var.amazon_linux_ami_id
   instance_type          = var.ec2_instance_type
@@ -11,14 +5,13 @@ resource "aws_instance" "data_migrate_ec2" {
   vpc_security_group_ids = [var.app_server_security_group_id, var.eice_security_group_id]
   iam_instance_profile   = var.iam_instance_profile
 
-  user_data = base64encode(templatefile("${path.module}/user_data_template.sh.tpl", {
-    rds_endpoint = var.rds_endpoint
-    rds_db_name  = var.rds_db_name
-    username     = var.username
+  user_data = base64encode(templatefile("/Users/safo/nest-docker-web-project/user_data_template.sh.tpl", {
+    rds_endpoint = var.rds_endpoint,
+    rds_db_name  = var.rds_db_name,
+    username     = var.username,
     password     = var.password
   }))
 
-  
   depends_on = [aws_db_instance.database_instance]
 
   tags = {
